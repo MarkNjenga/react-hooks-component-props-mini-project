da37e85230c1390386ead0fa4f3d51c682f9f524
Mark Njenga <marknjenga25@gmail.com>
2024-08-03 07:15:12 +0300

    Corrected code

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,16 @@
 import React from "react";
 import blogData from "../data/blog";
-import Header from "./Header"
-import About from "./About"
 import ArticleList from "./ArticleList";
-
+import About from "./About";
+import Header from "./Header";
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-    <Header name={blogData.name}/>
-    <About image={blogData.image} about={blogData.about}/>
-    <ArticleList posts={blogData.posts}/>
+      <Header name={blogData.name} />
+      <About image={blogData.image} about={blogData.about} />
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
-import React from "react"
+import React from "react";
 
-function Header(props){
-    return (
-        <header>
-            <h1>{props.name}</h1>
-        </header>
-        )
+function Header({name}) {
+ 
+  return (
+    <div>
+      <h1>{name}</h1>
+    </div>
+  );
 }
+
 export default Header;
